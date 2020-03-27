ff0df2f471c9a34be327750e826b156b5bc00b1b
mirrorhanyu <mirrorhanyu@gmail.com>
2020-03-27 22:52:07 +0800

    run on mac os

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ for entry in fetch_youtube_feed_entries():
         download(entry.video_id)
         generate_video_with_subtitle()
         upload_files_to_drive_by(entry)
-        # upload_files_to_bilibili(entry)
+        upload_files_to_bilibili(entry)
         send_notify_email(f'[Youtube Downloaded & Uploaded] - {entry.title}')
         # sleep 30 seconds to avoid bilibili upload issue
         sleep(30)
