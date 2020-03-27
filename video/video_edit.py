7d4e4c665e3497a8c80f587d94b24e7f216580bb
mirrorhanyu <mirrorhanyu@gmail.com>
2020-03-27 21:50:08 +0800

    fix 'SubtitlesClip' object has no attribute 'margin'

--- video/video_edit.py
+++ video/video_edit.py
@@ -12,7 +12,7 @@ def add_subtitle(video_path, subtitle_path, filename=None):
     generator: Callable[[Any], TextClip] = lambda txt: TextClip(txt, font='../fonts/GothamMedium.ttf',
                                                                 fontsize=35, color='white',
                                                                 bg_color='#00000066')
-    subtitle = SubtitlesClip(subtitle_path, generator).margin(bottom=15, opacity=0).set_position(('center', 'bottom'))
+    subtitle = SubtitlesClip(subtitle_path, generator).set_position(('center', 'bottom'))
     video = VideoFileClip(video_path, audio=True)
     composed_video = CompositeVideoClip([video, subtitle])
     composed_video.write_videofile(filename or add_prefix_to_filename(video_path, '[with-subtitle] '),
