c89cb9c850cda05a36cc4e5adc142c7e10351e23
mirrorhanyu <mirrorhanyu@gmail.com>
2020-03-28 19:48:55 +0800

    add Chinese name for bilibili title

--- bilibili/bilibili.py
+++ bilibili/bilibili.py
@@ -11,7 +11,7 @@ source = 'http://www.youtube.com'
 
 
 def upload_files_to_bilibili(entry):
-    title = entry.title_with_80_limit
+    title = f'#崔娃今夜秀# {entry.title}'.split(" | ")[0][:80]
     video_paths = [path for path in glob.glob('download/*') if is_video(path)]
     video_to_be_uploaded = next((path for path in video_paths if '[WITH-SUBTITLE]' in path), video_paths[0])
     bilibili_upload.upload(parts=[VideoPart(path=video_to_be_uploaded, title=title, desc=entry.media_description)],
