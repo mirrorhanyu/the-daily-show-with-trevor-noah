c5f957daba089813493a9754c6415e4278d4ba0a
mirrorhanyu <mirrorhanyu@gmail.com>
2020-03-28 11:57:14 +0800

    remove margin

--- video/video_edit.py
+++ video/video_edit.py
@@ -13,7 +13,7 @@ def add_subtitle(video_path, subtitle_path, filename=None):
                                                                 font='assets/font/GothamMedium.ttf',
                                                                 fontsize=35, color='white',
                                                                 bg_color='#00000066')
-    subtitle = SubtitlesClip(subtitle_path, generator).margin(bottom=15, opacity=0).set_position(('center', 'bottom'))
+    subtitle = SubtitlesClip(subtitle_path, generator).set_position(('center', 'bottom'))
     video = VideoFileClip(video_path, audio=True)
     composed_video = CompositeVideoClip([video, subtitle])
     output_filename = filename or replace_extension(add_prefix_to_filename(video_path, '[WITH-SUBTITLE] '), '.mp4')
