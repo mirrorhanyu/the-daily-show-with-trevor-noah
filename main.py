dedbd063f94d471cb2a6ad13051e5d391c78dd3a
mirrorhanyu <mirrorhanyu@gmail.com>
2020-03-30 19:32:04 +0800

    handle videos within 30 minutes

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from time import sleep
 from bilibili.bilibili import upload_files_to_bilibili
 from google.google import send_notify_email, upload_files_to_drive_by
 from utils.file_utils import remove_folder
-from utils.time_utils import is_within_hours
+from utils.time_utils import is_within_minutes
 from video.video import generate_video_with_subtitle
 from youtube.youtube import fetch_youtube_feed_entries, download
 
 for entry in fetch_youtube_feed_entries():
-    if is_within_hours(entry.published, hours=0.5):
+    if is_within_minutes(entry.published, minutes=30):
         download(entry.video_id)
         generate_video_with_subtitle()
         upload_files_to_drive_by(entry)
--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
 
 
-def is_within_hours(iso_datetime, hours=4):
-    return datetime.fromisoformat(iso_datetime) + timedelta(hours=hours) >= datetime.now(timezone.utc)
+def is_within_minutes(iso_datetime, minutes=30):
+    return datetime.fromisoformat(iso_datetime) + timedelta(minutes=minutes) >= datetime.now(timezone.utc)
