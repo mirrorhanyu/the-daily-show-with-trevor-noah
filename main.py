bda1dc3dbe3d2046a63a3ce8c272bd5f398ab35e
mirrorhanyu <mirrorhanyu@gmail.com>
2020-03-31 19:07:38 +0800

    get latest videos first

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from utils.time_utils import is_within_minutes
 from video.video import generate_video_with_subtitle
 from youtube.youtube import fetch_youtube_feed_entries, download
 
-for entry in fetch_youtube_feed_entries():
-    if is_within_minutes(entry.published, minutes=30):
-        download(entry.video_id)
-        generate_video_with_subtitle()
-        upload_files_to_drive_by(entry)
-        upload_files_to_bilibili(entry)
-        send_notify_email(f'[Youtube Downloaded & Uploaded] - {entry.title}')
-        # sleep 30 seconds to avoid bilibili upload issue
-        sleep(30)
-        remove_folder('download')
+latest_youtube_entries = [entry for entry in fetch_youtube_feed_entries() if is_within_minutes(entry.published, minutes=30)]
+for entry in latest_youtube_entries:
+    download(entry.video_id)
+    generate_video_with_subtitle()
+    upload_files_to_drive_by(entry)
+    upload_files_to_bilibili(entry)
+    send_notify_email(f'[Youtube Downloaded & Uploaded] - {entry.title}')
+    # sleep 30 seconds to avoid bilibili upload issue
+    sleep(30)
+    remove_folder('download')
