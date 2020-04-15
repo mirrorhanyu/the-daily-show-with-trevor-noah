bc704c29db758986fb88694f184c5525f8999c4f
mirrorhanyu <mirrorhanyu@gmail.com>
2020-04-15 21:15:04 +0800

    add subtitles

--- video/video.py
+++ video/video.py
@@ -6,7 +6,7 @@ from video.video_edit import add_subtitle
 
 def generate_video_with_subtitle():
     video_path = next(path for path in glob.glob('download/*') if is_video(path))
-    subtitle_path = next((path for path in glob.glob('download/*.en-US.srt') if is_subtitle(path)), None)
+    subtitle_path = next((path for path in glob.glob('download/*.srt') if is_subtitle(path)), None)
     # add subtitle only when en-US.srt existed
     if subtitle_path:
         add_subtitle(video_path, subtitle_path)
