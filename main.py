8cf8bd4325592255acf051795fa83e6b91237374
mirrorhanyu <mirrorhanyu@gmail.com>
2020-04-17 19:35:31 +0800

    for missed videos

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from time import sleep
 from bilibili.bilibili import upload_files_to_bilibili
 from google.google import send_notify_email, upload_files_to_drive_by
 from utils.file_utils import remove_folder
-from utils.time_utils import is_within_hours
 from video.video import generate_video_with_subtitle
 from youtube.youtube import fetch_youtube_feed_entries, download
 
-ENOUGH_HOURS_TO_WAIT_FOR_YOUTUBE_SUBTITLE = 6
+missed_video_ids = ['NcSUF8erpfU', 'jm3gAS3QqBg', 'Mvzjwi1W-Ro', 'UoOA69m4jwI', 'PpV_5-tCS-c', 'tofT7iqWzGk', 'CdOnqt5koKg']
 
-latest_youtube_entries = [entry for entry in fetch_youtube_feed_entries() if is_within_hours(entry.published, hours=ENOUGH_HOURS_TO_WAIT_FOR_YOUTUBE_SUBTITLE)]
+latest_youtube_entries = [entry for entry in fetch_youtube_feed_entries() if entry.video_id in missed_video_ids]
 for entry in reversed(latest_youtube_entries):
     download(entry.video_id)
     generate_video_with_subtitle()
