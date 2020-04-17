e09c309d3d13b40c5be500b18a3524bf9d2ecdf4
mirrorhanyu <mirrorhanyu@gmail.com>
2020-04-17 19:44:47 +0800

    for missed videos 1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 from bilibili.bilibili import upload_files_to_bilibili
-from google.google import send_notify_email, upload_files_to_drive_by
+from google.google import send_notify_email
 from utils.file_utils import remove_folder
 from video.video import generate_video_with_subtitle
 from youtube.youtube import fetch_youtube_feed_entries, download
@@ -12,7 +12,7 @@ latest_youtube_entries = [entry for entry in fetch_youtube_feed_entries() if ent
 for entry in reversed(latest_youtube_entries):
     download(entry.video_id)
     generate_video_with_subtitle()
-    upload_files_to_drive_by(entry)
+    # upload_files_to_drive_by(entry)
     upload_files_to_bilibili(entry)
     send_notify_email(f'[Youtube Downloaded & Uploaded] - {entry.title}')
     # sleep 30 seconds to avoid bilibili upload issue
