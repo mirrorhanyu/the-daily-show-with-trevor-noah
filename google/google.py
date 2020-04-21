3d6a7aedb6f3dfacc94cb195d6015f006b04df07
mirrorhanyu <mirrorhanyu@gmail.com>
2020-04-21 21:56:23 +0800

    comment google drive

--- google/google.py
+++ google/google.py
@@ -1,16 +1,16 @@
 import glob
 
-from google.google_drive import google_drive
+# from google.google_drive import google_drive
 from google.google_email import send_email
 from settings import settings
 
 daily_show_folder_id = '1vAn92qcBDwaZLc7dbqqdaqwSbszJw1cw'
 
 
-def upload_files_to_drive_by(entry):
-    folder = google_drive.create_folder_inside(parent_folder=daily_show_folder_id, sub_folder_name=entry.title)
-    for file in glob.glob('download/*'):
-        google_drive.upload(file_name=file, description=entry.google_drive_details, folder=folder)
+# def upload_files_to_drive_by(entry):
+#     folder = google_drive.create_folder_inside(parent_folder=daily_show_folder_id, sub_folder_name=entry.title)
+#     for file in glob.glob('download/*'):
+#         google_drive.upload(file_name=file, description=entry.google_drive_details, folder=folder)
 
 
 def send_notify_email(subject):
