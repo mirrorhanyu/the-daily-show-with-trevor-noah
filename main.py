46fceeb250825987948a6c60289e615985391ea4
mirrorhanyu <mirrorhanyu@gmail.com>
2020-07-14 17:53:43 +0800

    try

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from time import sleep
 from bilibili.bilibili import upload_files_to_bilibili
 from google.google import send_notify_email
 from utils.file_utils import remove_folder
-from utils.time_utils import is_within_hours
 from video.video import generate_video_with_subtitle
 from youtube.youtube import fetch_youtube_feed_entries, download
 
 ENOUGH_HOURS_TO_WAIT_FOR_YOUTUBE_SUBTITLE = 6
 
-latest_youtube_entries = [entry for entry in fetch_youtube_feed_entries() if is_within_hours(entry.published, hours=ENOUGH_HOURS_TO_WAIT_FOR_YOUTUBE_SUBTITLE)]
+latest_youtube_entries = [entry for entry in fetch_youtube_feed_entries()][:2]
 for entry in reversed(latest_youtube_entries):
     download(entry.video_id)
     generate_video_with_subtitle()
