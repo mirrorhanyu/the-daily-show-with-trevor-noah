aa96c754f96092b0dff5e8b3bc2026498a81174e
mirrorhanyu <mirrorhanyu@gmail.com>
2020-11-07 23:43:04 +0800

    fix

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -1,5 +1,4 @@
 import requests
-from you_get.extractors import youtube
 
 from youtube.youtube_feed import YoutubeFeed
 
@@ -8,7 +7,3 @@ def fetch_youtube_feed_entries():
     youtube_feeds_url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCwWhs_6x42TyRM4Wstoq8HA'
     feeds_xml = requests.get(youtube_feeds_url).text
     return YoutubeFeed(feeds_xml).entries
-
-
-def download(video_id):
-    youtube.download(url=f'https://www.youtube.com/watch?v={video_id}', output_dir='download', merge=True, caption=True)
