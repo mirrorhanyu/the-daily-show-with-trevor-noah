45feacb840713fff91803e24f81a3f45f7184a47
mirrorhanyu <mirrorhanyu@gmail.com>
2020-11-07 23:54:58 +0800

    add subtitle

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import youtube_dl
+from moviepy.video.VideoClip import TextClip
+from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
+from moviepy.video.fx.margin import margin
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.video.tools.subtitles import SubtitlesClip
 
 from youtube.youtube import fetch_youtube_feed_entries
 
@@ -11,3 +16,14 @@ with youtube_dl.YoutubeDL({
     'subtitle': '--write-sub --sub-lang en',
 }) as ydl:
     ydl.download([f'https://www.youtube.com/watch?v={entries[2].video_id}'])
+    video_path = 'youtube-download-file'
+    subtitle_path = 'youtube-download-file.en.srt'
+    generator = lambda txt: TextClip(txt,
+                                     font='assets/font/GothamMedium.ttf',
+                                     fontsize=45, color='white', bg_color='#00000066')
+    subtitle = margin(clip=SubtitlesClip(subtitle_path, generator)
+                      .set_position(('center', 'bottom')), bottom=35, opacity=0)
+    video = VideoFileClip(video_path, audio=True)
+    composed_video = CompositeVideoClip([video, subtitle])
+    output_filename = 'youtube-output-file'
+    composed_video.write_videofile(output_filename, threads=2, fps=video.fps)
