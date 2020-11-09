42cd6f32e447329263cc97d43f449e0d0e37ff3c
mirrorhanyu <mirrorhanyu@gmail.com>
2020-11-09 22:53:24 +0800

    try try

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ WORKDIR /app
 
 COPY . .
 
+RUN apt-get update && apt-get install -y imagemagick
+
 RUN ["chmod", "+x", "./docker-entrypoint.sh"]
 
 ENTRYPOINT ["./docker-entrypoint.sh"]
--- main.py
+++ main.py
@@ -1,35 +1,27 @@
 import glob
+import os
 
-import youtube_dl
 from moviepy.video.VideoClip import TextClip
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 from moviepy.video.fx.margin import margin
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from moviepy.video.tools.subtitles import SubtitlesClip
 
-from libs.vtt_to_srt import vtt_to_srt
-from utils.file_utils import is_vtt_subtitle
 from youtube.youtube import fetch_youtube_feed_entries
 
 entries = fetch_youtube_feed_entries()
 
-with youtube_dl.YoutubeDL({
-    'outtmpl': 'youtube-download-file',
-    'writesubtitles': True,
-    'subtitlesformat': 'srt',
-    'subtitle': '--sub-lang en --write-sub --convert-subs srt',
-}) as ydl:
-    ydl.download([f'https://www.youtube.com/watch?v={entries[2].video_id}'])
-    video_path = 'youtube-download-file'
-    subtitle_path = next((path for path in glob.glob('youtube-download-file.en.*')), None)
-    if is_vtt_subtitle(subtitle_path):
-        vtt_to_srt(subtitle_path)
-        subtitle_path = 'youtube-download-file.en.srt'
-    generator = lambda txt: TextClip(txt,
-                                     font='assets/font/GothamMedium.ttf',
-                                     fontsize=45, color='white', bg_color='#00000066')
-    subtitle = clip=SubtitlesClip(subtitle_path, generator).set_position(('center', 'bottom'))
-    video = VideoFileClip(video_path, audio=True)
-    composed_video = CompositeVideoClip([video, subtitle])
-    output_filename = 'youtube-output-file'
-    composed_video.write_videofile(output_filename, threads=2, fps=video.fps)
+os.system(f'you-get --output-filename youtube-download-file https://www.youtube.com/watch?v={entries[2].video_id}')
+video_path = next((path for path in glob.glob('youtube-download-file.*')), None)
+subtitle_path = next((path for path in glob.glob('*.srt')), None)
+print('video_path', video_path)
+print('subtitle_path', subtitle_path)
+generator = lambda txt: TextClip(txt,
+                                 font='assets/font/GothamMedium.ttf',
+                                 fontsize=45, color='white', bg_color='#00000066')
+subtitle = margin(clip=SubtitlesClip(subtitle_path, generator)
+                  .set_position(('center', 'bottom')), bottom=35, opacity=0)
+video = VideoFileClip(video_path, audio=True)
+composed_video = CompositeVideoClip([video, subtitle])
+output_filename = 'youtube-output-file'
+composed_video.write_videofile(output_filename, threads=2, fps=video.fps)
